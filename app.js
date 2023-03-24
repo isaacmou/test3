dd79ec3069ff9f576bc1e31476dceb68f4f50c4a
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 16:19:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function showEnglish() {
     document.getElementById("engVer").style.display = "none";
-    document.getElementById ("btn1").style.display="none";
     document.getElementById("chiVer").style.display = "none";
     document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
     document.getElementById("engInput").style.display = "";
